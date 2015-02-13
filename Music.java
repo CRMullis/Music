258166c466289364322d2077c0eb76c03ed47e17
Collin Mullis <cmullis@saintjoe.edu>
2015-02-13 16:42:25 -0500

    Added final constructor and added a new album from my frined Halie's list of songs.

---
+++ Music.java
@@ -0,0 +1,40 @@
+public class Music {
+
+// Class to keep track of music
+// CMP112 Collin Mullis 13 February 2015
+
+	public String album;
+	public String artist;
+	public String genre;
+	public String media;
+	public String location;
+	public int copies;
+
+/* ----------------------------------------------------------------*/
+
+	Music () {
+		album = "No album";
+		artist = "No artist";
+		genre = "No genre";
+		media = "None";
+		location = "Nowhere";
+		copies = 0;
+		}
+	Music (String thisAlbum, String thisArtist, String thisGenre, String thisMedia) {
+		this();
+		album = thisAlbum;
+		artist = thisArtist;
+		genre = thisGenre;
+		media = thisMedia;
+		}
+	Music (String thisAlbum, String thisArtist) {
+		this();
+		album = thisAlbum;
+		artist = thisArtist;
+		}		
+	public String toString() {
+		String output = "";
+		output = "Album: " + album + " Artist: " + artist + " Genre: " + genre + " Media: " + media + " Location: " + location + " Copies: " + copies;
+		return output;
+		}
+}
---
+++ MusicDriver.java
@@ -0,0 +1,11 @@
+public class MusicDriver {
+	public static void main(String[] args) {
+		Music myMusic = new Music();
+		System.out.println(myMusic.toString());
+		Music anotherMusic = new Music("Back in Black", "AC/DC", "Metal", "CD");
+		System.out.println(anotherMusic.toString());
+		Music haliesMusic = new Music("Alibis", "Mona");
+		System.out.println(haliesMusic.toString());
+	}
+}
+
